d5dd438fa94da5d80da50b5d15c4ef695c114bc4
LRYP <2213469156@qq.com>
2025-07-03 13:54:43 +0800

    fix: semester service init & wrong process

--- src/main/scala/Process/Init.scala
+++ src/main/scala/Process/Init.scala
@@ -1,8 +1,7 @@
-
 package Process
 
 import Common.API.{API, PlanContext, TraceID}
-import Common.DBAPI.{initSchema, writeDB}
+import Common.DBAPI.{initSchema, writeDB, readDBInt}
 import Common.ServiceUtils.schemaName
 import Global.ServerConfig
 import cats.effect.IO
@@ -18,11 +17,12 @@ object Init {
     given DBConfig = server2DB(config)
 
     val program: IO[Unit] = for {
-      _ <- IO(GlobalVariables.isTest=config.isTest)
+      _ <- IO(GlobalVariables.isTest = config.isTest)
       _ <- API.init(config.maximumClientConnection)
       _ <- Common.DBAPI.SwitchDataSourceMessage(projectName = Global.ServiceCenter.projectName).send
       _ <- initSchema(schemaName)
-            /** 学期阶段表，包含学期当前阶段及相关权限
+
+      /** 学期阶段表，包含学期当前阶段及相关权限
        * current_phase: 学期当前阶段（1代表阶段1，2代表阶段2）
        * allow_teacher_manage: 是否允许教师创建、删除、修改课程组和课程
        * allow_student_select: 是否允许学生选课
@@ -38,10 +38,27 @@ object Init {
             allow_student_drop BOOLEAN NOT NULL DEFAULT false,
             allow_student_evaluate BOOLEAN NOT NULL DEFAULT false
         );
-         
         """,
         List()
       )
+
+      // 检查表是否已有条目
+      count <- readDBInt(
+        s"""SELECT COUNT(*) FROM "${schemaName}"."semester_phase_table";""",
+        List()
+      )
+
+      // 如果为空，则插入默认条目
+      _ <- if (count == 0)
+        writeDB(
+          s"""
+          INSERT INTO "${schemaName}"."semester_phase_table"
+          (current_phase, allow_teacher_manage, allow_student_select, allow_student_drop, allow_student_evaluate)
+          VALUES (1, false, false, false, false);
+          """,
+          List()
+        )
+      else IO.unit
     } yield ()
 
     program.handleErrorWith(err => IO {
@@ -50,4 +67,3 @@ object Init {
     })
   }
 }
-    
